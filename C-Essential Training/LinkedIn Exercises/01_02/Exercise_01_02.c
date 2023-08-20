570e29ec9874302f7b44a8b5db81726c7ece0928
Harmanpreet Singh Sagar <harmanpreet1308@gmail.com>
2023-08-20 01:48:12 -0400

    Move files into new folder

---
+++ C-Essential Training/LinkedIn Exercises/01_02/Exercise_01_02.c
@@ -0,0 +1,13 @@
+// Brings in the stdio header file into the code.
+#include <stdio.h>
+
+// Main running function
+int main()
+{
+  // puts or printf is used to print something out to cmd.
+  puts("Hey my name is Harman!");
+  puts("How are you?\n");
+  // Returning 0 means that we are satisfied and would like to continue
+  // essentially
+  return 0;
+}
