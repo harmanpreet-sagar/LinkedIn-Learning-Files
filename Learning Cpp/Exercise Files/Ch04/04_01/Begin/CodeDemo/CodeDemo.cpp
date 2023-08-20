622b807465d0b9ae705004c13179ab4c97edcd43
Harmanpreet Singh Sagar <110783820+harmanpreet-sagar@users.noreply.github.com>
2023-08-20 01:54:55 -0400

    Upload Exercise files from Chapter 4

---
+++ Learning Cpp/Exercise Files/Ch04/04_01/Begin/CodeDemo/CodeDemo.cpp
@@ -0,0 +1,11 @@
+// Learning C++ 
+// Exercise 04_01
+// If Statements
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    return (0);
+}
---
+++ Learning Cpp/Exercise Files/Ch04/04_03/Begin/CodeDemo/CodeDemo.cpp
@@ -0,0 +1,16 @@
+// Learning C++ 
+// Exercise 04_03
+// While Loops
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> numbers = {12,25,31,47,58};
+
+int main(){
+    vector<int>::iterator ptr = numbers.begin();
+    
+    return (0);
+}
