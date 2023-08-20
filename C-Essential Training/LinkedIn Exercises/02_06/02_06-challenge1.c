b6dd5902ba57f978df65a59740583e01df490bf0
Harmanpreet Singh Sagar <110783820+harmanpreet-sagar@users.noreply.github.com>
2023-08-20 02:05:43 -0400

    Update 02_06-challenge1.c

--- C-Essential Training/LinkedIn Exercises/02_06/02_06-challenge1.c
+++ C-Essential Training/LinkedIn Exercises/02_06/02_06-challenge1.c
@@ -3,14 +3,13 @@
 int main()
 {
   int a = 5;
-  // Use single quotes when assigning a char value as double quotes indicate a
-  // String.
+  // Use single quotes when assigning a char value as double quotes indicate a String.
 
   // Since this is a constant and we should not be able to modify it later, we
   // need to use the const keyword to indicate that it is a constant. Using that
   // keyword will prevent us from modifying it later.
 
-  // We need to immediately assign a value to a variable that has the const type.
+  // We need to immediately assign a value to a variable with the const type.
   const char b = 'B';
 
 	printf("Here is the value of A: %d\n",a);
